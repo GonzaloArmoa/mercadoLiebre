fa2dcc767da38d53a1a8b9cb0b1b6ef592bc172e
GonzaloArmoa <gonzalo-armoa@hotmail.com>
2023-08-14 22:33:48 -0300

    feat: wip implementation of routes and controllers

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+// ************ Require's ************
+const express = require('express');
+const router = express.Router();
+
+// ************ Controller Require ************
+const indexController = require('../controllers/indexController');
+
+
+router.get('/', indexController.index); 
+router.get('/search', indexController.search); 
+
+module.exports = router;
